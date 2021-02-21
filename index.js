4e5797a1bf71d724592ac2d96fc5d4723cff116d
gooin <gooin@outlook.com>
2021-02-22 00:41:29 +0800

    doc: 更新使用说明

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 let geojson_encode = require('./src/geojson-encode')
 let sort_migration_json = require('./src/sort-migration-json')
 
-// // 压缩 geojson 为 pbf
-// 运行： node index.js
+/**
+ * 用过的的处理GIS数据的函数
+ * 函数调用的入口，根据需要注释启用对应的代码块即可
+ * 运行： node index.js
+ */
+
+/**
+ * 压缩 geojson 为 pbf
+ */
 // const geojsonFile = './example-datas/point_100_for_heatmap_geojson.json';
 // geojson_encode(geojsonFile,'./example-datas/point_100_for_heatmap_geojson.pbf')
 
 
-// 抽取数据
+/**
+ * 分类json数据为geojson
+ */
 const jsonFile = './example-datas/jiangsu_migration.json';
 const savedFile = './example-datas/jiangsu_migration_sorted_by_city_geojson.json';
 sort_migration_json(jsonFile, savedFile);
